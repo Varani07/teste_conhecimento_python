6073e1289954de09508ff5165143add2157cac97
Varani07 <matheusvarani@gmail.com>
2024-10-23 14:33:34 -0300

    Loja comentada.

--- loja.py
+++ loja.py
@@ -1,15 +1,21 @@
 import random
 
 class Loja():
+    #TODO 
     def __init__(self, nome:str):
+        #* Nome, lista com os produtos da loja
+        #TODO Melhorar!
         self.nome = nome
         self.produtos = []
 
     def adicionar_a_loja(self, item:object):
+        #* Adiciona um item a loja
         self.produtos.append(item)
 
     @property
     def ver_produtos(self):
+        #* Mostra os produtos da loja
+        #TODO Melhorar e implementar formatação para outros itens
         id = 1
         print()
         print(f"- - - - {self.nome} - - - -")
@@ -21,6 +27,7 @@ class Loja():
         print("- - - - - - - - - - - - - - - -")
 
     def adicionar_produtos_carrinho_de_compras(self, num_serie:str):
+        #* Adiciona um item da loja ao carrinho de compras se o número de série for encontrado
         for item in self.produtos:
             if item.num_de_serie == num_serie:
                 print()
@@ -30,9 +37,13 @@ class Loja():
         print("Item nao encontrado!")
         return None
 
-    # def definir_codigo_de_serie(self, item):
-    #     if item.tipo == "Computador":
-    #         num_serie = ""
-    #         for _ in range(1, 11):
-    #             num_serie += str(random.randint(1, 10))
-    #     return num_serie
+    def definir_codigo_de_serie(self, item):
+        #* Gera um número de série para os produtos da loja
+        #! Testar
+        if item.tipo == "Computador":
+            num_serie = "C"
+            for volta in range(1, 6):
+                num_serie += str(random.randint(1, 9))
+                if volta == 4:
+                    num_serie += "-"
+        return num_serie
