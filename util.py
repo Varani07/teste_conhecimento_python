92548694f6f3b479bf7d48c4db8f180abafb92c5
Varani07 <matheusvarani@gmail.com>
2024-10-23 14:48:36 -0300

    util ducumentado

--- util.py
+++ util.py
@@ -3,6 +3,8 @@ import json
 from pessoa import Pessoa
 from caixas_texto import *
 def get_nicks():
+        #* Seleciona o nome dos jogadores fazendo uso de set() para não repetir
+        #* as informações, utilizado em @estrutura.py -> @load_game
         get_nicks = DAO()
         nicks = get_nicks.visualizar('nick', 'player_info', "", "", False)
         nicks = {nick[0] for nick in nicks}
@@ -10,18 +12,26 @@ def get_nicks():
         return nicks
 
 def get_saves_by_nick(nick):
+        #* Seleciona todos os saves de um jogador em ordem data>data, utilizado
+        #* em @estrutura.py -> @escolha_save
         get_nicks = DAO()
         nicks = get_nicks.visualizar('id, dados, data_do_save, tipo', 'player_info', " WHERE nick = %s ORDER BY data_do_save DESC", (nick,), False)
 
         return nicks
 
 def get_save_by_id(id):
+        #* Seleciona os dados do save escolhido, utilizado em
+        #* @estrutura.py -> @escolha_save
         get_nicks = DAO()
         nicks = get_nicks.visualizar('dados, tipo', 'player_info', " WHERE id = %s", (id,), False)
 
         return nicks
 
 def desempacotar_json(dados:json, tipo:str):
+        #* Recebe do banco de dados as informações do player em JSON,
+        #* transforma as mesmas em um dict() e após isso transfere seus
+        #* valores para um objeto de sua respectiva classe.
+        #! Se os atributos de alguma classe mudar será necessária a manutenção dessa função
         info_player = json.loads(dados)
         if tipo == 'Pessoa':
                 player = Pessoa(nome=info_player['nome'])
